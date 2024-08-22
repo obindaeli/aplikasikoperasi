2f68249a3e52a81342b2db8827688d62493eda65
obindaeli <devkominfonisbar@gmail.com>
2024-08-22 18:44:27 +0700

    Edit Nama Aplikasi

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -11,7 +11,7 @@
                  
                   <img src="<?=base_url('assets/')?>img/banner.png" alt="niasbarat" width="100%" height="100%">
                   <h3 class="text-primary">E-KOPERASI NB</h3>
-                  <h4>SISTEM PENDATAAN KOPERASI NIAS BARAT</h4>
+                  <h4>APLIKASI KOPERASI NIAS BARAT</h4>
                   <h6>DINAS KETENAGA KERJAAN, KOPERASI DAN UKM NIAS BARAT</h6>
                   
                   
